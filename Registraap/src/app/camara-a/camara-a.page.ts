98451a9ffe85017c973ddf893d0fdbfb54908d2b
nicolas <n.luna@duocuc.cl>
2022-09-22 23:17:05 -0300

    nicolas
    Validaciones

---
+++ Registraap/src/app/camara-a/camara-a.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camara-a',
+  templateUrl: './camara-a.page.html',
+  styleUrls: ['./camara-a.page.scss'],
+})
+export class CamaraAPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
